7da02603fb4473b0921d8a583a502b07e3683ccb
galtimur <gal_timur@yahoo.com>
2025-05-20 14:17:23 +0200

    Initial commit

--- codi_model.py
+++ codi_model.py
@@ -1,13 +1,24 @@
-import json
 import torch
-import pandas as pd
 import torch.nn as nn
-import torch.optim as optim
-from tqdm import tqdm
 from peft import LoraConfig, get_peft_model
-from torch.utils.data import Dataset, DataLoader
-from transformers import AutoTokenizer, AutoModelForCausalLM, get_cosine_schedule_with_warmup
+from transformers import (
+    AutoTokenizer,
+    AutoModelForCausalLM,
+    get_cosine_schedule_with_warmup,
+)
+
 
 class CODIModel(nn.Module):
-
-    def __init__(self, model_path, icot_length=6, alpha=1, beta=1, gamma=1, max_length=256):
+    def __init__(
+        self, model_path, icot_length=6, alpha=1, beta=1, gamma=1, max_length=256
+    ):
+        self.max_length = max_length
+        self.icot_length = icot_length
+        self.tokenizer = AutoTokenizer.from_pretrained(model_path)
+        self.tokenizer.add_special_tokens(
+            {"additional_special_tokens": ["<bot>", "<eot>"]}
+        )
+        self.tokenizer.pad_token = self.tokenizer.eos_token
+        self.model = AutoModelForCausalLM.from_pretrained(
+            model_path, torch_dtype=torch.bfloat16
+        )
