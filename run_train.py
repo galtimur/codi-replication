42b11a3b791bc70ecc2906442782664c33ae3e9e
galtimur <gal_timur@yahoo.com>
2025-05-21 12:21:35 +0200

    Added wandb login

--- run_train.py
+++ run_train.py
@@ -27,7 +27,7 @@ def setup_seed(seed=0xDEADC0DE, cudnn_benchmark=True):
 
 
 def train(config: DictConfig) -> None:
-    # wandb.login(key=os.environ["WANDB_KEY"], host=os.environ["WANDB_HOST"])
+    wandb.login(key=os.environ["WANDB_KEY"], host=os.environ["WANDB_HOST"])
     wandb.init(
         project="codi",
         name=os.environ["WANDB_NAME"],
